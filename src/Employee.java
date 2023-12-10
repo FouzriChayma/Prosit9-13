4700d1daaa23fed5287bd494ca6c8cc591d9f612
FouzriChayma <chayma.fouzri@esprit.tn>
2023-12-10 11:43:28 +0100

    Initial commit for Prosit 11

---
+++ src/Employee.java
@@ -0,0 +1,18 @@
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@ToString
+public class Employee {
+    private int id;
+    private String name;
+    private String address;
+    private int phoneNumber;
+    private String email;
+
+}
