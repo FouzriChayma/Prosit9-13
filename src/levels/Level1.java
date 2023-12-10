195e6c3fc8df083c5e3cbbe49099019f4225eb63
FouzriChayma <chayma.fouzri@esprit.tn>
2023-12-10 13:49:11 +0100

    Correct Level3

--- src/levels/Level1.java
+++ src/levels/Level1.java
@@ -1,8 +1,10 @@
 package levels;
 
+import models.Subject;
 import models.Teacher;
 import utils.Data;
 
+import java.util.Comparator;
 import java.util.List;
 
 public class Level1 {
@@ -10,42 +12,31 @@ public class Level1 {
     public static void main(String[] args) {
         List<Teacher> employees = Data.employees();
 
-        /*
-         * TO DO 1: Afficher tous les enseignants
-         */
+        // TO DO 1: Afficher tous les enseignants
         employees.forEach(System.out::println);
 
-        /*
-         * TO DO 2: Afficher les enseignants dont le nom commence par la lettre n
-         */
+        // TO DO 2: Afficher les enseignants dont le nom commence par la lettre n
         employees.stream().filter(t -> t.getName().startsWith("n")).forEach(System.out::println);
 
-        /*
-         * TO DO 3: Afficher les enseignants dont le nom commence par la lettre n et le salaire > 100000
-         */
+        // TO DO 3: Afficher les enseignants dont le nom commence par la lettre n et le salaire > 100000
         employees.stream().filter(t -> t.getName().startsWith("n") && t.getSalary() > 100000)
                 .forEach(System.out::println);
 
-        /*
-         * TO DO 4: Afficher les enseignants JAVA triés par salaire (éliminer les redondances)
-         */
-        employees.stream().filter(t -> "JAVA".equalsIgnoreCase(t.getSubject())).distinct()
-                .sorted((t1, t2) -> Double.compare(t1.getSalary(), t2.getSalary())).forEach(System.out::println);
+        // TO DO 4: Afficher les enseignants JAVA triés par salaire (éliminer les redondances)
+        employees.stream().distinct().filter(t -> t.getSubject() == Subject.JAVA)
+                .sorted(Comparator.comparingInt(Teacher::getSalary).reversed())
+                .forEach(System.out::println);
 
-        /*
-         * TO DO 5: Afficher les noms des enseignants dont le salaire > 60000 avec 2 manières différentes
-         */
-
+        // TO DO 5: Afficher les noms des enseignants dont le salaire > 60000 avec 2 manières différentes
         /* First Way */
         employees.stream().filter(t -> t.getSalary() > 60000).map(Teacher::getName).forEach(System.out::println);
         /* Second Way */
         employees.stream().filter(t -> t.getSalary() > 60000).forEach(t -> System.out.println(t.getName()));
 
-        /*
-         * TO DO 6:  Ajouter 200 Dt pour les enseignants dont le nom commence par m et afficher celui qui a le salaire le plus élevé
-         */
+        // TO DO 6: Ajouter 200 Dt pour les enseignants dont le nom commence par m et afficher celui qui a le salaire le plus élevé
         Teacher highestPaidTeacher = employees.stream().filter(t -> t.getName().startsWith("m"))
-                .peek(t -> t.setSalary(t.getSalary() + 200)).max((t1, t2) -> Double.compare(t1.getSalary(), t2.getSalary()))
+                .peek(t -> t.setSalary(t.getSalary() + 200))
+                .max(Comparator.comparingInt(Teacher::getSalary))
                 .orElse(null);
 
         System.out.println("Highest Paid Teacher after the increase: " + highestPaidTeacher);
--- src/levels/Level3.java
+++ src/levels/Level3.java
@@ -1,8 +1,9 @@
 package levels;
 
 import models.Teacher;
-import utils.Data;
+import models.Subject;
 
+import utils.Data;
 import java.util.*;
 import java.util.stream.Collectors;
 
@@ -11,37 +12,36 @@ public class Level3 {
     public static void main(String[] args) {
         List<Teacher> teachers = Data.employees();
 
-        /* TO DO 1: Retourner une chaîne de caractères qui contient tous les noms des enseignants en majuscule séparés par # */
-        String names = teachers.stream().map(Teacher::getName)
-                .map(name -> name == null ? "" : name.toUpperCase())
+        // TO DO 1: Retourner une chaîne de caractères qui contient tous les noms des enseignants en majuscule séparés par #
+        String names = teachers.stream()
+                .map(Teacher::getName)
+                .map(String::toUpperCase)
                 .collect(Collectors.joining("#"));
         System.out.println("Concatenated names in uppercase: " + names);
 
-        /* TO DO 2: Retourner un set d'enseignants Java dont le salaire > 80000 */
-        Set<Teacher> teachers1 = teachers.stream().filter(t ->
-                        "Java".equalsIgnoreCase(t.getSubject()) && t.getSalary() != null && t.getSalary() > 80000)
+        // TO DO 2: Retourner un set d'enseignants Java dont le salaire > 80000
+        Set<Teacher> javaTeachersWithHighSalary = teachers.stream()
+                .filter(t -> t.getSubject() == Subject.JAVA && t.getSalary() > 80000)
                 .collect(Collectors.toSet());
-        System.out.println("Java teachers with salary > 80000: " + teachers1);
+        System.out.println("Java teachers with salary > 80000: " + javaTeachersWithHighSalary);
 
-        /* TO DO 3: Retourner un TreeSet d'enseignants (tri par nom et en cas d'égalité tri par salaire) */
-        TreeSet<Teacher> teachers2 = teachers.stream()
-                .filter(t -> t.getName() != null && t.getSalary() != null)
-                .collect(Collectors.toCollection(() -> new TreeSet<>(Comparator
-                        .comparing(Teacher::getName)
-                        .thenComparing(Teacher::getSalary))));
-        System.out.println("Teachers TreeSet sorted by name and salary: " + teachers2);
+        // TO DO 3: Retourner un TreeSet d'enseignants (tri par nom et en cas d'égalité tri par salaire)
+        TreeSet<Teacher> teachersSortedByNameAndSalary = teachers.stream()
+                .collect(Collectors.toCollection(() ->
+                        new TreeSet<>(Comparator
+                                .comparing(Teacher::getName)
+                                .thenComparingInt(Teacher::getSalary))));
+        System.out.println("Teachers TreeSet sorted by name and salary: " + teachersSortedByNameAndSalary);
 
-        /* TO DO 4: Retourner une Map qui regroupe les enseignants par module */
-        Map<String, List<Teacher>> map = teachers.stream()
-                .filter(t -> t.getSubject() != null)
+        // TO DO 4: Retourner une Map qui regroupe les enseignants par module
+        Map<Subject, List<Teacher>> teachersGroupedBySubject = teachers.stream()
                 .collect(Collectors.groupingBy(Teacher::getSubject));
-        System.out.println("Teachers grouped by subject: " + map);
+        System.out.println("Teachers grouped by subject: " + teachersGroupedBySubject);
 
-        /* TO DO 5: Retourner une Map qui regroupe les noms des enseignants par salaire */
-        Map<Double, String> map1 = teachers.stream()
-                .filter(t -> t.getName() != null && t.getSalary() != null)
+        // TO DO 5: Retourner une Map qui regroupe les noms des enseignants par salaire
+        Map<Integer, String> namesGroupedBySalary = teachers.stream()
                 .collect(Collectors.groupingBy(Teacher::getSalary,
                         Collectors.mapping(Teacher::getName, Collectors.joining(", "))));
-        System.out.println("Names grouped by salary: " + map1);
+        System.out.println("Names grouped by salary: " + namesGroupedBySalary);
     }
 }
